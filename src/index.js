bbad800bb31274a45d74025fdc70a72348f1980e
Manvith_Reddy <manvithreddy@gmail.com>
2023-12-13 15:28:59 -0500

    Amplify Project

--- src/index.js
+++ src/index.js
@@ -9,16 +9,16 @@ import { Amplify } from 'aws-amplify'
 import config from './aws-exports'
 
 // src/index.js
-import { AmplifyProvider } from '@aws-amplify/ui-react'
+import { ThemeProvider } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
 
 
 Amplify.configure(config)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <AmplifyProvider  theme={studioTheme}>
+  <ThemeProvider theme={studioTheme}>
     <App />
-  </AmplifyProvider>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
